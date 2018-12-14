a4d538ce7460479f33b9de7086b4c56fd47fea64
Masashi Sutou <sutou.masasi@gmail.com>
2018-12-15 01:58:21 +0900

    Initial Commit

---
+++ NavigationDrawerSampler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationDrawerSampler
+//
+//  Created by 須藤将史 on 2018/12/15.
+//  Copyright © 2018 須藤将史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
